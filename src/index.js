97caab11c1ec4f85d17086f3f4b96867b1b16775
Weston Farmer <wesfarmer11@gmail.com>
2024-01-18 08:45:44 -0600

    renderRamens adds images to div ramen-menu

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 // write your code here 
+const ramenContainer = document.getElementById('ramen-menu');
+
 function getRamens() {
     fetch("http://localhost:3000/ramens")
     .then(resp => resp.json())
-    .then(data => console.log(data));
+    .then(data => data.forEach(renderRamens));
 }
 getRamens()
+//fetch ramens
+//create img for each ramen and append it to the DOM 
+//renderRamens will replace the console.log within getRamens
 
+function renderRamens(ramen) {
+    const ramenImg = document.createElement('img');
+    ramenImg.src = ramen.image
+    ramenImg.alt = `${ramen.name} image`;
+
+    ramenContainer.append(ramenImg);
+
+}
+
+//render details for ramen when clicked (add) and append to div ""
