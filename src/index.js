2162561bfa6152a4b1e697856bb4b8fc6062e2a9
Weston Farmer <wesfarmer11@gmail.com>
2024-01-18 12:04:48 -0600

    first attempt at moveToDetails function

--- src/index.js
+++ src/index.js
@@ -8,16 +8,29 @@ function getRamens() {
 }
 getRamens()
 //fetch ramens
-//create img for each ramen and append it to the DOM 
-//renderRamens will replace the console.log within getRamens
+//create img for each ramen and append it to the DOM in ramen-menu 
+
 
 function renderRamens(ramen) {
     const ramenImg = document.createElement('img');
     ramenImg.src = ramen.image
     ramenImg.alt = `${ramen.name} image`;
+    
+    ramenImg.addEventListener('click', () => moveToDetails(ramen))
 
     ramenContainer.append(ramenImg);
 
 }
 
-//render details for ramen when clicked (add) and append to div ""
+//render details for ramen when clicked and append to div ""
+
+//build out callback function to append ramenImg to #ramen-detail, takes the ramen object that was clicked as argument
+function moveToDetails(ramen) { 
+    const ramenName = document.getElementsByClassName('name')
+    ramenName.textContent = ramen.name
+    const ramenImage = document.getElementsByClassName('detail-image')
+    ramenImage.src = ramen.image
+    const ramenRestaurant = document.getElementsByClassName('restaurant')
+    ramenRestaurant.textContent = ramen.restaurant
+
+}
